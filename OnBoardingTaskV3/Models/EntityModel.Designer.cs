3fa83e746cba2dacfee44609a99855d00568d2d7
paramB <param.bhangal04@gmail.com>
2018-12-27 12:41:10 +1300

    added data model diagram image and database script

--- OnBoardingTaskV3/Models/EntityModel.Designer.cs
+++ OnBoardingTaskV3/Models/EntityModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\IndustryConnect\OnboardingTaskForProject\OnBoardingTaskV3\OnBoardingTaskV3\OnBoardingTaskV3\Models\EntityModel.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\GitHub\CRUD-Repository-Pattern-Asp.Net-MVC-KnockoutJs-EF\OnBoardingTaskV3\Models\EntityModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
